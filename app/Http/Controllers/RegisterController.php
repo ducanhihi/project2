3160f9df16539e334c5e9f19d356092d40b66008
Duc anh <duccanhd04@gmail.com>
2024-03-13 21:55:10 +0700

    hiển thị thông báo khi đăng kí trùng email đã tồn tại

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -13,20 +13,30 @@ class RegisterController extends Controller
     {
         return view('auth.register');
     }
-    public function register(Request $request)
-    {
-        $user = new User();
-        $user->name = $request->input('name');
-        $user->email = $request->input('email');
-        $user->phone = $request->input('phone');
-        $user->password = bcrypt($request->input('password'));
-        $user->address = $request->input('address');
-        $user->role = 'customer';
-        $user->save();
-        return redirect() ->route('login');
+    public function register(Request $request) {
+        try {
+            $existingUser = User::where('email', $request->input('email'))->first();
+            if ($existingUser) {
+                // Email đã tồn tại trong cơ sở dữ liệu
+                // Hiển thị thông báo lỗi trên trang register
+                return redirect()->back()->withErrors(['error' => 'Email đã tồn tại trong hệ thống!']);
+            } else {
+                // Email chưa tồn tại, tiến hành tạo người dùng mới
+                $user = new User();
+                $user->name = $request->input('name');
+                $user->email = $request->input('email');
+                $user->phone = $request->input('phone');
+                $user->password = Hash::make($request->input('password'));
+                $user->address = $request->input('address');
+                $user->role = 'customer';
+                $user->save();
 
-        // Redirect to a success page
-//        return redirect()->route('registration.success');
+                return redirect()->route('login')->with('success', 'Người dùng đã được tạo thành công.');
+            }
+        } catch (Exception $e) {
+            // Xử lý ngoại lệ nếu có
+            echo 'Đã xảy ra lỗi: ' . $e->getMessage();
+        }
     }
 
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,3 +1,10 @@
+<div>
+@if ($errors->any())
+    <div class="alert alert-danger">
+        {{ $errors->first() }}
+    </div>
+@endif
+</div>
 <!doctype html>
 <html lang="en">
 <head>
@@ -27,11 +34,11 @@
 </head>
 <body>
 <script src="https://cdn.ckeditor.com/ckeditor5/41.1.0/classic/ckeditor.js"></script>
-<section class="vh-100" style="background-color: #eee;">
+<section>
     <div class="container h-100">
         <div class="row d-flex justify-content-center align-items-center h-100">
             <div class="col-lg-12 col-xl-11">
-                <div class="card text-black" style="border-radius: 25px;">
+                <div class="card bg-dark text-white" style="border-radius: 1rem;">
                     <div class="card-body p-md-5">
                         <div class="row justify-content-center">
                             <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
