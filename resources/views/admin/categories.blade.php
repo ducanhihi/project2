816c85182ed4ffbe60d3d166d9492fe8f2833d43
chiyeuminhemss <chiyeuminhemss>
2024-03-20 15:27:09 +0700

    pushdi

---
+++ resources/views/admin/categories.blade.php
@@ -0,0 +1,29 @@
+@extends('layout.app')
+
+@section('content')
+    <table class="table table-bordered table-hover">
+        <tr class="table-active text-center">
+            <th>ID</th>
+            <th>Name</th>
+            <th>Created_at</th>
+            <th>Update_at</th>
+        </tr>
+        @forelse($allCategories as $categories)
+            <tr>
+                <td class="text-center">{{$categories-> id}}</td>
+                <td class="fw-bold">{{$categories-> name}}</td>
+                <td class="fw-bold">{{$categories-> created_at}}</td>
+                <td class="fw-bold">{{$categories-> updated_at}}</td>
+{{--                                <td class="d-flex justify-content-around align-content-center">--}}
+{{--                                    <a class="btn btn-sm btn-warning" href="admin/home/{{$products->id}}/edit">Sửa</a>--}}
+{{--                                    <form onsubmit="return confirm('Ban co muon xoa')" action="/home/news/{{$products-> id}}" method="POST">--}}
+{{--                                        @method('DELETE')--}}
+{{--                                        @csrf--}}
+{{--                                        <button class="btn btn-sm btn-danger">Xoa</button>--}}
+{{--                                    </form>--}}
+{{--                                </td>--}}
+            </tr>
+        @empty
+        @endforelse
+    </table>
+@endsection
--- resources/views/admin/products.blade.php
+++ resources/views/admin/products.blade.php
@@ -1,29 +1,32 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Page</title>
-    <link rel="stylesheet" href="/bootstrap-5.3.3/css/bootstrap.min.css">
-</head>
-<body>
+@extends('layout.app')
+
+@section('content')
     <table class="table table-bordered table-hover">
         <tr class="table-active text-center">
-            <th>Code</th>
+            <th>ID</th>
+            <th>ISBNcode</th>
             <th>Name</th>
             <th>Price</th>
+            <th>Ngày tạo</th>
+            <th>Hành động</th>
         </tr>
         @forelse($allProducts as $products)
             <tr>
-                <td class="text-center">{{$products->isbn_code}}</td>
-                <td class="fw-bold text-center">{{$products->name}}</td>
-                <td class="text-center">{{$products->price}}</td>
+                <td class="text-center">{{$products-> id}}</td>
+                <td class="text-center">{{$products-> isbn_code}}</td>
+                <td class="fw-bold">{{$products-> name}}</td>
+                <td class="fw-bold">{{$products-> price}}</td>
+                <td>{{$products-> created_at}}</td>
+                                <td class="d-flex justify-content-around align-content-center">
+                                    <a class="btn btn-sm btn-warning" href="admin/home/{{$products->id}}/edit">Sửa</a>
+                                    <form onsubmit="return confirm('Ban co muon xoa')" action="/home/news/{{$products-> id}}" method="POST">
+                                        @method('DELETE')
+                                        @csrf
+                                        <button class="btn btn-sm btn-danger">Xoa</button>
+                                    </form>
+                                </td>
             </tr>
-
         @empty
         @endforelse
     </table>
-</body>
-</html>
+@endsection
