fbc7df10544c380e4be719232087eb2a9e5259c2
chiyeuminhemss <chiyeuminhemss>
2024-03-20 16:58:22 +0700

    aaa

--- resources/views/admin/products.blade.php
+++ resources/views/admin/products.blade.php
@@ -1,32 +1,38 @@
 @extends('layout.app')
 
 @section('content')
-    <table class="table table-bordered table-hover">
-        <tr class="table-active text-center">
-            <th>ID</th>
-            <th>ISBNcode</th>
-            <th>Name</th>
-            <th>Price</th>
-            <th>Ngày tạo</th>
-            <th>Hành động</th>
-        </tr>
-        @forelse($allProducts as $products)
-            <tr>
-                <td class="text-center">{{$products-> id}}</td>
-                <td class="text-center">{{$products-> isbn_code}}</td>
-                <td class="fw-bold">{{$products-> name}}</td>
-                <td class="fw-bold">{{$products-> price}}</td>
-                <td>{{$products-> created_at}}</td>
-{{--                                <td class="d-flex justify-content-around align-content-center">--}}
-{{--                                    <a class="btn btn-sm btn-warning" href="admin/home/{{$products->id}}/edit">Sửa</a>--}}
-{{--                                    <form onsubmit="return confirm('Ban co muon xoa')" action="/home/news/{{$products-> id}}" method="POST">--}}
-{{--                                        @method('DELETE')--}}
-{{--                                        @csrf--}}
-{{--                                        <button class="btn btn-sm btn-danger">Xoa</button>--}}
-{{--                                    </form>--}}
-{{--                                </td>--}}
-            </tr>
-        @empty
-        @endforelse
-    </table>
+    <div class="container">
+        <div class="row">
+{{--            <div class="col-10 offset-1">--}}
+                <table class="table table-bordered table-hover">
+                    <tr class="table-active text-center">
+                        <th>ID</th>
+                        <th>ISBNcode</th>
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th>Ngày tạo</th>
+                        <th>Hành động</th>
+                    </tr>
+                    @forelse($allProducts as $products)
+                        <tr>
+                            <td class="text-center">{{$products-> id}}</td>
+                            <td class="text-center">{{$products-> isbn_code}}</td>
+                            <td class="fw-bold">{{$products-> name}}</td>
+                            <td class="fw-bold">{{$products-> price}}</td>
+                            <td>{{$products-> created_at}}</td>
+                            {{--                                <td class="d-flex justify-content-around align-content-center">--}}
+                            {{--                                    <a class="btn btn-sm btn-warning" href="admin/home/{{$products->id}}/edit">Sửa</a>--}}
+                            {{--                                    <form onsubmit="return confirm('Ban co muon xoa')" action="/home/news/{{$products-> id}}" method="POST">--}}
+                            {{--                                        @method('DELETE')--}}
+                            {{--                                        @csrf--}}
+                            {{--                                        <button class="btn btn-sm btn-danger">Xoa</button>--}}
+                            {{--                                    </form>--}}
+                            {{--                                </td>--}}
+                        </tr>
+                    @empty
+                    @endforelse
+                </table>
+            </div>
+{{--        </div>--}}
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BrandsController;
+use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
@@ -32,6 +34,16 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 Route::get('/register', [RegisterController::class, 'viewRegister'])->name('register');
 Route::post('/register', [RegisterController::class, 'register']);
+
+///product
 Route::get('admin/products', [ProductsController::class, 'viewAdminProducts'])->name('admin.products');
+
+//category
+Route::get('admin/categories', [CategoriesController::class, 'viewAdminCategories'])->name('admin.categories');
+
+//brands
+Route::get('admin/brands', [BrandsController::class, 'viewAdminBrands'])->name('admin.brands');
+
+
 
 // sua fil nay
